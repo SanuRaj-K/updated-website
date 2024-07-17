ee1165abf47c70361fda39825942cb79a1345c5c
SanuRaj-K <sanusrk29@gmail.com>
2024-07-17 16:40:43 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <header>
+        <div>
+          <h1>
+           
+            <h1>&lt;SRK/&gt;</h1>
+          </h1>
+        </div>
+        <div>
+          <ul className="">
+            <li>Home</li>
+            <li>About</li>
+            <li>Resume</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
