ccfd92ca6ee317f9948cf9a9a2a998c534a93105
SanuRaj-K <sanusrk29@gmail.com>
2024-07-18 15:36:47 +0530

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,15 @@ function About() {
       </div>
       <div>
         <h1>Curious About Me....? Here You Have It :)</h1>
-        <p>I am a passionate, self proclimed developer, who specialises in Full Stack Developement (MERN)</p>
+        <p>
+          I am a passionate, self proclimed developer, who specialises in Full
+          Stack Developement (MERN). I am very enthusiastic about bringing the
+          technical and visual aspects of digital products to life. User
+          experience, pixcel perfect design and write clear, readable, highly
+          perfomant code matters to me <br />
+          I began my Journy as a Frontend Developer intern in 2022, and since then, I've continued to grow and  evolve as a developer, taking on new challenges and learning the new technologies along the way. I am building cuting-edge web applications using modern technologies. <br />I am very  much a progressive thinker and enjoy working on products end to end, from ideation all the way  to development. <br />
+           
+        </p>
       </div>
     </div>
   );
