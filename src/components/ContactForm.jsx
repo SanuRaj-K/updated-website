1e21da0a93ec125a6beaaf625fd96e469efd6bcd
SanuRaj-K <sanusrk29@gmail.com>
2024-10-09 12:28:10 +0530

    feat:contact form added

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ContactForm() {
-    const handleSubmit =()=>{
-        
-    }
+  const [formValues, setFromValues] = useState({
+    email: "",
+    subject: "",
+    message: "",
+  });
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFromValues((prevValue) => ({
+      ...prevValue,
+      [id]: value,
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const whatsappNumber=+917025980439
+    const whatsappUrl=`https://wa.me/${whatsappNumber}?text=Email: ${formValues.email}%0ASubject: ${formValues.subject}%0AMessage: ${formValues.message}`
+    window.open(whatsappUrl,'_blank')
+  };
   return (
     <div>
       <section class="bg-white dark:bg-gray-900">
@@ -14,7 +29,7 @@ function ContactForm() {
           <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
             Feel Free to Reach Me...
           </p>
-          <form action="#" class="space-y-8">
+          <form action="#" onSubmit={handleSubmit} class="space-y-8">
             <div>
               <label
                 for="email"
@@ -23,6 +38,8 @@ function ContactForm() {
                 Your email
               </label>
               <input
+                onChange={handleChange}
+                value={formValues.email}
                 type="email"
                 id="email"
                 class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
@@ -38,6 +55,8 @@ function ContactForm() {
                 Subject
               </label>
               <input
+                onChange={handleChange}
+                value={formValues.subject}
                 type="text"
                 id="subject"
                 class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
@@ -54,6 +73,9 @@ function ContactForm() {
               </label>
               <textarea
                 id="message"
+                onChange={handleChange}
+                value={formValues.message}
+                required
                 rows="6"
                 class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Leave a comment..."
@@ -61,7 +83,6 @@ function ContactForm() {
             </div>
             <button
               type="submit"
-              onClick={handleSubmit}
               class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
             >
               Send message
