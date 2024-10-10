1fce8ae961cfa4ef863fdeb6fa1ffd45757bd23b
SanuRaj-K <sanusrk29@gmail.com>
2024-10-10 10:46:13 +0530

    feat:contact form added

--- src/App.js
+++ src/App.js
@@ -8,18 +8,20 @@ import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import ContactForm from "./components/ContactForm";
+import RoundAnimation from "./components/RoundAnimation";
 function App() {
   return (
     <div className="relative  bg-black">
       <Toaster position="top-right" reverseOrder={false} />
-      <Header id={''} />
+      <Header id={""} />
       <main className="py-8 bg-white  px-4">
         <Hero id={"home"} />
         <About id={"about"} />
         <Skills id={"skills"} />
+        <RoundAnimation />
         <Portfolio id={"projects"} />
         <Contact id={"contact"} />
-        <ContactForm/>
+        <ContactForm />
       </main>
       <Footer />
     </div>
--- src/components/RoundAnimation.jsx
+++ src/components/RoundAnimation.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function RoundAnimation() {
   return (
-    <div>RoundAnimation</div>
+    <div> </div>
   )
 }
 
